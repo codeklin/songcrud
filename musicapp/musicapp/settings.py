5177db3ccf35b45e50dc11966fd53941b5ffe283
Igbalaye Olajide <clickcom007@yahoo.com>
2022-10-21 14:15:16 +0100

    hot fix

--- musicapp/musicapp/settings.py
+++ musicapp/musicapp/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'musicapp'
+    'rest_framework',
+    'musicapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
