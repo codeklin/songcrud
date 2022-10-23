9a37931c86a98b78e11f44a9f4897bc73ee3e195
Igbalaye Olajide <clickcom007@yahoo.com>
2022-10-23 22:31:18 +0100

    apps settings

--- musicapp/musicapp/models.py
+++ musicapp/musicapp/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 
-class Songs(models.Model):
-    name = models.Charfield(max_lenght=100000)
+
+class Album(models.Model):
+    artist = models.Charfield(max_lenght=200)
+    album_title = models.Charfield(max_lenght=300)
+    genre = models.Charfield(max_lenght=100)
+    album_logo = models.Charfield(max_lenght=1000)
+
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=10)
+    song_title = models. CharField(max_length=250)
--- musicapp/musicapp/settings.py
+++ musicapp/musicapp/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
-    'musicapp',
+    'musicapp.models.Album',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
