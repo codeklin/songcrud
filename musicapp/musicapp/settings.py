12a258c82dae852c10adafb39ea9d1034e0efb90
Igbalaye Olajide <clickcom007@yahoo.com>
2022-10-24 07:22:14 +0100

    admin fix

--- musicapp/musicapp/settings.py
+++ musicapp/musicapp/settings.py
@@ -31,8 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
-    'musicapp.models.Album',
+    'musicapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
