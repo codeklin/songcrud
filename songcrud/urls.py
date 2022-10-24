1257a5ce6bc5fd411b385d9f14cf66ce9bdf1b6e
igbalaye olajide <clickcomo007@yahoo.com>
2022-10-25 00:03:25 +0100

    template fix

--- songcrud/urls.py
+++ songcrud/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('Home page')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('musicapp.urls'))
+
 ]
