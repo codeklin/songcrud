3989263de7112e5566a4708f84bbb1d4ba45366a
igbalaye olajide <clickcomo007@yahoo.com>
2022-10-25 13:03:32 +0100

    add admin

--- musicapp/models.py
+++ musicapp/models.py
@@ -6,16 +6,16 @@ from django.db import models
 class Artiste(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=200)
-    age = models.IntegerField(max_length=100)
+    age = models.IntegerField()
 
 
-class Song(models.Models):
+class Song(models.Model):
     title = models.CharField(max_length=100)
     date_released = models.DateField(max_length=100)
     likes = models.CharField(max_length=100)
-    artiste_id = models.IntegerField(max_length=100)
+    artiste_id = models.IntegerField()
 
 
 class Lyric(models.Model):
     content = models.CharField(max_length=5000)
-    song_id = models.IntegerField(max_length=100)
+    song_id = models.IntegerField()
