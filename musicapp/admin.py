ad7a834bd727a8275dc0c093d1aafc12fa17fc0f
igbalaye olajide <clickcomo007@yahoo.com>
2022-10-25 16:58:43 +0100

    models to admin

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Artiste, Song, Lyric
+
+
 # Register your models here.
 
 
--- musicapp/models.py
+++ musicapp/models.py
@@ -8,12 +8,19 @@ class Artiste(models.Model):
     last_name = models.CharField(max_length=200)
     age = models.IntegerField()
 
+    def __str__(self):
+        return self.first_name
+
 
 class Song(models.Model):
+    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     date_released = models.DateField(max_length=100)
-    likes = models.CharField(max_length=100)
+    likes = models.CharField(max_length=100000000)
     artiste_id = models.IntegerField()
+
+    def __str__(self):
+        return self.title
 
 
 class Lyric(models.Model):
