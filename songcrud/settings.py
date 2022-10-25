10946c95affdd1e422f8cccdde1c7bfc5b343d97
igbalaye olajide <clickcomo007@yahoo.com>
2022-10-25 07:27:52 +0100

    fixes

--- songcrud/settings.py
+++ songcrud/settings.py
@@ -46,7 +46,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates'
+
         ],
         'APP_DIRS': True,
         'OPTIONS': {
